3cd1eabbb61786b973396aa61eee4700d581a6d8
Justintime50 <justinpaulhammond@gmail.com>
2020-09-30 17:48:10 -0600

    Bumping Fibonacci Sequence and added test

--- formula/fibonacci_sequence.rb
+++ formula/fibonacci_sequence.rb
@@ -4,11 +4,15 @@
 class FibonacciSequence < Formula
   desc 'The Fibonacci Sequence printed to the CLI.'
   homepage 'https://github.com/Justintime50/fibonacci-sequence'
-  url 'https://github.com/Justintime50/fibonacci-sequence/archive/v1.0.0.tar.gz'
-  version 'v1.0.0'
-  sha256 '49c626dcb3f1d7b105e023f54fd6b985e0091d9138a6c57df13496915845ddab'
+  url 'https://github.com/Justintime50/fibonacci-sequence/archive/v1.0.1.tar.gz'
+  version 'v1.0.1'
+  sha256 '8c9d76f640566854b57e82053046a208b0f690663286f07a2b54420d7f425727'
 
   bottle :unneeded
+
+  test do
+    assert_match '0, 1, 1, 2, 3, 5', shell_output('fibonacci-sequence 6')
+  end
 
   def install
     bin.install 'src/fibonacci-sequence.sh' => 'fibonacci-sequence'
