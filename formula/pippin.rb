c43be0822a274af7d3121feaee85781e651cc1c7
Justintime50 <justinpaulhammond@gmail.com>
2020-11-12 16:53:02 -0700

    Updating Pippin v0.2.0

--- formula/pippin.rb
+++ formula/pippin.rb
@@ -1,8 +1,8 @@
 class Pippin < Formula
   desc "Get the dependency tree of a Python virtual environment"
   homepage "https://github.com/Justintime50/pippin"
-  url "https://github.com/Justintime50/pippin/archive/v0.1.0.tar.gz"
-  sha256 "73d948f7078bb3ed273249040351f10bb57445408d39a0daa8e41829c14eee47"
+  url "https://github.com/Justintime50/pippin/archive/v0.2.0.tar.gz"
+  sha256 "b5c2cba35367c6ae3c5388ab588659678d7aeb84e1b6dbada24c0f8e6e3e0e01"
   bottle :unneeded
 
   def install
