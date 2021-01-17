57278209e5c59e79b4630160f4e6a3fac3faa3c9
Justintime50 <justinpaulhammond@gmail.com>
2021-01-16 18:25:24 -0700

    Brew formula update for freedom version v0.4.1

--- formula/freedom.rb
+++ formula/freedom.rb
@@ -5,20 +5,20 @@
 class Freedom < Formula
   desc "Give your computer the freedom to do more"
   homepage "https://github.com/Justintime50/freedom"
-  version "0.4.0"
+  version "0.4.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/Justintime50/freedom/releases/download/v0.4.0/freedom_0.4.0_darwin_amd64.tar.gz"
-    sha256 "aeab7af560fd51c76933828cd1f10273010670e6ab10189535fd33331693f47c"
+    url "https://github.com/Justintime50/freedom/releases/download/v0.4.1/freedom_0.4.1_darwin_amd64.tar.gz"
+    sha256 "2d4889abd46a3580eb257cb1facbb216379da5d65bab2eb1562e8ab71f23bf97"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/Justintime50/freedom/releases/download/v0.4.0/freedom_0.4.0_linux_amd64.tar.gz"
-    sha256 "8ccd684263a833754ee71bf9686c8d8e2db6f0b45f467127496dd8ac8b5e5068"
+    url "https://github.com/Justintime50/freedom/releases/download/v0.4.1/freedom_0.4.1_linux_amd64.tar.gz"
+    sha256 "e8c977d9487bfff65e57fae6bc23a3ab8f6cf2ab14e3f3cd6bdbd302780c66a0"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/Justintime50/freedom/releases/download/v0.4.0/freedom_0.4.0_linux_arm64.tar.gz"
-    sha256 "e423ff062906e06704f432ca8dfb9ce4a76796bc2b81214c1e520f1e92cd97b6"
+    url "https://github.com/Justintime50/freedom/releases/download/v0.4.1/freedom_0.4.1_linux_arm64.tar.gz"
+    sha256 "c49691dec6ba1d8704bb38cc6dbab33e8f439d257b0f9f63c5f4d0ad3851dfdf"
   end
 
   def install
