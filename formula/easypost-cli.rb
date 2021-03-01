7fc5e878adb81e70b2ba35667075859f79e834e9
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-03-01 21:45:39 +0000

    Brew formula update for easypost-cli version v1.3.2

--- formula/easypost-cli.rb
+++ formula/easypost-cli.rb
@@ -5,8 +5,8 @@
 class EasypostCli < Formula
   desc "Easypost api on the cli"
   homepage "https://github.com/Justintime50/easypost-cli"
-  url "https://github.com/Justintime50/easypost-cli/archive/v1.3.1.tar.gz"
-  sha256 "931cd86160a6e5872d923bcb8c9fa86d41215accee77c72a4d4bdf28ca6b0b85"
+  url "https://github.com/Justintime50/easypost-cli/archive/v1.3.2.tar.gz"
+  sha256 "6de0be7c20451d600e688fc024f4f3a199e74133f595101d4320baf4279cb653"
   license "MIT"
   bottle :unneeded
 
