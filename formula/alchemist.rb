9368de159ec8f789c0fc9840c65b0a62b7440502
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-04-09 20:14:37 -0600

    Brew formula update for alchemist version v2.2.1

--- formula/alchemist.rb
+++ formula/alchemist.rb
@@ -5,24 +5,24 @@
 class Alchemist < Formula
   desc "Update, backup, and administer your Homebrew or Chocolatey instances"
   homepage "https://github.com/Justintime50/alchemist"
-  version "2.2.0"
+  version "2.2.1"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/Justintime50/alchemist/releases/download/v2.2.0/alchemist_2.2.0_darwin_amd64.tar.gz"
-    sha256 "e3ee5ab0f130bf5e217ec1dbe3462ffc289eefff6d55b6149ec9124708f83b24"
+    url "https://github.com/Justintime50/alchemist/releases/download/v2.2.1/alchemist_2.2.1_darwin_amd64.tar.gz"
+    sha256 "7295da49ad3c43664b595379665c6bc9ac75dfaa9f12992eccf3ead474b36f71"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/Justintime50/alchemist/releases/download/v2.2.0/alchemist_2.2.0_darwin_arm64.tar.gz"
-    sha256 "c7a15a7ad645bba9df82aa57b5b2bfb8386ecc96dbe84df47ecb0b0bc35c8798"
+    url "https://github.com/Justintime50/alchemist/releases/download/v2.2.1/alchemist_2.2.1_darwin_arm64.tar.gz"
+    sha256 "be2896a238bf9ab928e8963fe0031a6536e6d0b92c0d3a84389d381dca45588b"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/Justintime50/alchemist/releases/download/v2.2.0/alchemist_2.2.0_linux_amd64.tar.gz"
-    sha256 "ca210e7dafa45733ce94793d85353b497b84c8562bbba29794d3ac5c968f9bc8"
+    url "https://github.com/Justintime50/alchemist/releases/download/v2.2.1/alchemist_2.2.1_linux_amd64.tar.gz"
+    sha256 "83dab53a41413e1125cefb04271be1a7f6e87197129cfd959db119706eb97a74"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/Justintime50/alchemist/releases/download/v2.2.0/alchemist_2.2.0_linux_arm64.tar.gz"
-    sha256 "9785b7df4c713e7e66db257ea368d3cc4e00a61016641a65ce9e6ae4e70cf8bf"
+    url "https://github.com/Justintime50/alchemist/releases/download/v2.2.1/alchemist_2.2.1_linux_arm64.tar.gz"
+    sha256 "ba7d60369b61c2724be43b5bf27365c0422bc5c7bd4f6a4c8d7c1d8ca4d8796d"
   end
 
   def install
