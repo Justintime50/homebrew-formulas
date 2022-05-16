f1a0e15fbf7d60e497ff70e8fc4c6ee540fb8942
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-05-16 00:16:33 -0600

    chore: add cronic tap

---
+++ formula/cronic.rb
@@ -0,0 +1,15 @@
+# typed: false
+# frozen_string_literal: true
+
+# This formula was manually created and simply packages up a 3rd party shell script for easier distribution.
+class Cronic < Formula
+  desc "A cure for Cron's chronic email problem"
+  homepage "https://github.com/justincase/cronic"
+  url "https://github.com/Justintime50/cronic/archive/v2.tar.gz"
+  sha256 "d625576815b11a408c1ae3ba7dd0d81923181728c15cc57f1ff134655c671511"
+  license "MIT"
+
+  def install
+    bin.install "cronic" => "cronic"
+  end
+end
