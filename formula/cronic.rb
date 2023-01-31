c098bd0905052ff932ee0c9a8215f5d1a7299293
Justin Hammond <39606064+Justintime50@users.noreply.github.com>
2023-01-31 12:01:20 -0700

    Update cronic.rb

--- formula/cronic.rb
+++ formula/cronic.rb
@@ -3,7 +3,7 @@
 
 # This formula was manually created and simply packages up a 3rd party shell script for easier distribution.
 class Cronic < Formula
-  desc "A cure for Cron's chronic email problem"
+  desc "Cure for Cron's chronic email problem"
   homepage "https://github.com/justincase/cronic"
   url "https://github.com/Justintime50/cronic/archive/v2.tar.gz"
   sha256 "d625576815b11a408c1ae3ba7dd0d81923181728c15cc57f1ff134655c671511"
