bbc4d9a824575e9507bafb393158d6dc0a290125
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2025-03-27 05:16:38 +0000

    chore: brew formula update for srvinfra v2.0.0

--- Formula/srvinfra.rb
+++ Formula/srvinfra.rb
@@ -5,8 +5,8 @@
 class Srvinfra < Formula
   desc "Is a tool to deploy, update, and maintain services and websites on a se"
   homepage "https://github.com/Justintime50/srvinfra"
-  url "https://github.com/Justintime50/srvinfra/archive/refs/tags/v1.2.1.tar.gz"
-  sha256 "28b737d30077ac1b1e3c06ce91ea932363dbc57649cd9f9c883e1829be06d90c"
+  url "https://github.com/Justintime50/srvinfra/archive/refs/tags/v2.0.0.tar.gz"
+  sha256 "ee51f777c13370f1b8bcbec27b6d1ad9646f119c4239e5e6cf7c7209acd95af8"
   license "MIT"
 
   def install
